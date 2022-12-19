7dbb52dabd36cda9bffce33123261cb003680827
feliperodsdev <libertyisrequired@gmail.com>
2022-12-19 09:54:14 -0300

    feat: express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("testando");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log("Server is running!"));
