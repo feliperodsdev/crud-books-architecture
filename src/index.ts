0c6b631ad41cf25a9a54bece7561603b7c03dc0e
feliperodsdev <libertyisrequired@gmail.com>
2022-12-19 12:50:11 -0300

    feat: Mongo Connection

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,25 @@ import express from "express";
 import { config } from "dotenv";
 import { GetBooksController } from "./controllers/get-users/get-books";
 import { MongoGetBooksRespository } from "./repositories/get-books/mongo-get-books";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
+  const app = express();
 
-const app = express();
+  await MongoClient.connect();
 
-const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 5000;
 
-app.get("/books", async (req, res) => {
-  const mongoGetBooksRespository = new MongoGetBooksRespository();
-  const getBookController = new GetBooksController(mongoGetBooksRespository);
+  app.get("/books", async (req, res) => {
+    const mongoGetBooksRespository = new MongoGetBooksRespository();
+    const getBookController = new GetBooksController(mongoGetBooksRespository);
 
-  const response = await getBookController.handle();
+    const response = await getBookController.handle();
 
-  res.status(response.statusCode).send(response.body);
-});
+    res.status(response.statusCode).send(response.body);
+  });
 
-app.listen(port, () => console.log("Server is running!"));
+  app.listen(port, () => console.log("Server is running!"));
+};
+main();
